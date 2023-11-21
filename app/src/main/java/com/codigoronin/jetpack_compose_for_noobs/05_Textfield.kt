9b0e01d2d6dd7dd6c800f7f9e57aa6549772b5a5
Ronin <codigoronin@gmail.com>
2023-11-21 09:24:31 +0100

    OutlinedTextfield

--- app/src/main/java/com/codigoronin/jetpack_compose_for_noobs/05_Textfield.kt
+++ app/src/main/java/com/codigoronin/jetpack_compose_for_noobs/05_Textfield.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -15,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,7 +56,32 @@ fun MytextfieldAdvance() {
                         it
             },
             label = { Text("Nombre") },
+
             placeholder = { Text(text = "Introduce tu nombre") }
+
+        )
+    }
+}
+
+@Composable
+fun MyTextfiedOutlined() {
+    var myText by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        OutlinedTextField(
+            value =myText,
+            onValueChange ={myText = it},
+            label = { Text("Introduce tu nombre") },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Green,
+                unfocusedBorderColor = Color.Red,
+                focusedLabelColor= Color.Cyan
+            )
         )
     }
 }
